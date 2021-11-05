77682236fc0c70ea1f793e665996f1182397cff9
waldekglaz <waldekglaz@gmail.com>
2021-11-05 15:31:20 +0000

    input clearing after submiting user number, css

--- script.js
+++ script.js
@@ -4,16 +4,18 @@ const number = Math.floor(Math.random() * 100);
 const myButton = document.querySelector('.btn');
 
 const playerChoice = parseInt(input.value)
-console.log(number)
+
 let counter = 0;
 
 myButton.addEventListener('click', function(){
     if(parseInt(input.value) > number){
         msg.textContent = `The secret number is smaller than ${parseInt(input.value)}`
         counter++;
+        input.value = '';
     }else if(parseInt(input.value) < number){
         msg.textContent = `The secret number is bigger than ${parseInt(input.value)}`
         counter++;
+        input.value = '';
     }else if(parseInt(input.value) === number && counter === 0){
         msg.textContent = `You've won! The secret number was ${number} and you had ${counter + 1} guess`;
     }else if(parseInt(input.value) === number){
@@ -23,5 +25,4 @@ myButton.addEventListener('click', function(){
     }else{
         msg.textContent = `Please type a number!`
     }
-    
 })
