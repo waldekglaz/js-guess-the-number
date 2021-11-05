c25ccd6e91d50943dcd94b4304c6562284f951c7
waldekglaz <waldekglaz@gmail.com>
2021-11-05 06:49:02 +0000

    condition when empty input, basic css

--- script.js
+++ script.js
@@ -4,21 +4,24 @@ const number = Math.floor(Math.random() * 100);
 const myButton = document.querySelector('.btn');
 
 const playerChoice = parseInt(input.value)
-
+console.log(number)
 let counter = 0;
 
 myButton.addEventListener('click', function(){
     if(parseInt(input.value) > number){
-        msg.textContent = `${parseInt(input.value)} is bigger than the secret number!`
+        msg.textContent = `The secret number is smaller than ${parseInt(input.value)}`
         counter++;
     }else if(parseInt(input.value) < number){
-        msg.textContent = `${parseInt(input.value)} is smaller than the secret number!`
+        msg.textContent = `The secret number is bigger than ${parseInt(input.value)}`
         counter++;
     }else if(parseInt(input.value) === number && counter === 0){
         msg.textContent = `You've won! The secret number was ${number} and you had ${counter + 1} guess`;
-    }else{
+    }else if(parseInt(input.value) === number){
         counter++;
         msg.textContent = `You've won! The secret number was ${number} and you had ${counter} guesses`;
+        counter = 0;
+    }else{
+        msg.textContent = `Please type a number!`
     }
     
 })
